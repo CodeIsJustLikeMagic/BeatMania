90fa08a79b71c6bced1950c418a9dd6da7479102
CodeIsJustLikeMagic <CodeIsJustLikeMagic@web.de>
2020-12-01 22:21:02 +0100

    added a Simpel Beat Ceator to Test with and a Beat Listener (Unity Event)

---
+++ BeatManiUnity/Assets/Scripts/Beat/FakeBeat.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.Events;
+
+//test class that creates Beat Event once every Second
+//This class should be replaced by a proper beat system
+public class FakeBeat : MonoBehaviour
+{
+    public UnityEvent beat;
+    public float timeBetweenBeats = 1.0f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        InvokeRepeating("CreateEvent", 0, timeBetweenBeats);
+    }
+
+    void CreateEvent()
+    {
+        Debug.Log("Beat triggered");
+        beat.Invoke();
+    }
+    
+}
---
+++ BeatManiUnity/Assets/Scripts/BeatListenerBehavior.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+using UnityEngine.Events;
+
+public class BeatListenerBehavior : MonoBehaviour
+{
+
+    public void BeatReaction()
+    {
+        Debug.Log("Beat recieved");
+    }
+}
