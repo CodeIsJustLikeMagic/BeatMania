2ba9d88f489b4389427f49efaeeb1a191e5862c9
CodeIsJustLikeMagic <CodeIsJustLikeMagic@web.de>
2020-12-01 23:45:02 +0100

    created two Attack Animations and Animation Script that starts them
    Player has an Attack Animations Script
    Press e or r to run a white or blue attack animation

---
+++ BeatManiUnity/Assets/Scripts/AttackAnimations.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AttackAnimations : MonoBehaviour
+{
+    Animator animator;
+    // Start is called before the first frame update
+    void Start()
+    {
+        animator = GetComponentInChildren<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown("e"))//white normal attack 1 (first attack on beat)
+        {
+            animator.SetTrigger("whiteN1");
+
+        }
+
+        if (Input.GetKeyDown("r"))
+        {
+            animator.SetTrigger("blueN1");
+        }
+    }
+}
