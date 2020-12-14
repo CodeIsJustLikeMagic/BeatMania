7ea84f36a2341a887b5b2edd75a599182869a02f
DennisSchmidt23 <49645755+DennisSchmidt23@users.noreply.github.com>
2020-12-14 18:19:16 +0100

    AudioHelm Import
    imported AudioHelm
    added Dennis TestScene

---
+++ Assets/Scripts/AudioHelm/NoteOnBounce.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NoteOnBounce : MonoBehaviour
+{
+    public AudioHelm.HelmController helmController;
+    public int note = 60;
+    public float subVolume = 0.0f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        helmController.SetParameterPercent(AudioHelm.Param.kSubVolume, subVolume);
+        helmController.NoteOn(note, 1.0f, 0.5f);
+    }
+}
