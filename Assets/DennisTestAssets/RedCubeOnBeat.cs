55f913c0e871c9d261385b47b47a766cc9d9f0af
DennisSchmidt23 <49645755+DennisSchmidt23@users.noreply.github.com>
2020-12-28 19:54:33 +0100

    Added Sequences, did Testing

---
+++ Assets/DennisTestAssets/RedCubeOnBeat.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class RedCubeOnBeat : MonoBehaviour
+{
+    public Material beatCubeMat;
+    private int counter = 0;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    public void showBeat()
+    {
+        counter++;
+        if(counter%2 == 0)
+        {
+            beatCubeMat.color = new Color(1f, 0f, 0f);
+        }
+        else
+        {
+            beatCubeMat.color = new Color(1f, 1f, 1f);
+        }
+        
+    }
+}
