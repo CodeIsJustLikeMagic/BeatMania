e5b5ec3d604a55a3a90ce79a56ad08f8593929c3
CodeIsJustLikeMagic <CodeIsJustLikeMagic@web.de>
2021-01-09 20:09:27 +0100

    sprite change system for songchange

---
+++ Assets/Scripts/EasyParallax/Background.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Background : MonoBehaviour
+{
+    public static Background instance;
+    public SpriteRenderer[] renderers;
+    // Start is called before the first frame update
+    private void Awake()
+    {
+        instance = this;
+    }
+    public void Start()
+    {
+        renderers = gameObject.GetComponentsInChildren<SpriteRenderer>();
+        SetSprites("yellow");
+    }
+    public void SetSprites(string color)
+    {
+        foreach(SpriteRenderer rend in renderers)
+        {
+            string str = "yellow" + rend.gameObject.name;
+            Sprite sprt = Resources.Load<Sprite>("BackgroundTextures/yellow" + rend.gameObject.name);
+            rend.sprite = sprt;
+        }
+    }
+}
