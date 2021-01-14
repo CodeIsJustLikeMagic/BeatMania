4849d22efbe82a03d642449f608108c23e5a9ab9
DennisSchmidt23 <49645755+DennisSchmidt23@users.noreply.github.com>
2021-01-14 15:47:53 +0100

    Added Songs (in test scene)

---
+++ Assets/Scripts/AudioHelm/EnemyOneSequence.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyOneSequence : MonoBehaviour
+{
+    AudioHelm.Sequencer sequencer;
+    public int offset = 0;
+    //startingNote ranges from 0 to 127
+    // Start is called before the first frame update
+    void Start()
+    {
+        sequencer = this.GetComponent<AudioHelm.Sequencer>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.E))
+        {
+            sequencer.Clear();
+        }
+        else if (Input.GetKeyDown(KeyCode.R))
+        {
+            sequencer.Clear();
+            sequencer.AddNote(70 + offset, 0, 1);
+            sequencer.AddNote(82 + offset, 2,4);
+
+
+        }
+    }
+}
