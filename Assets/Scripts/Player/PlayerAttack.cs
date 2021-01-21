cc5ef52b65b6c112fb599bf6cc47f15f9c7e36a8
Janselepoor <jan-felix@hotmail.com>
2021-01-21 11:41:46 +0100

    Controller Prototyp + Animationen aus Metroidvania Controller

---
+++ Assets/Scripts/Player/PlayerAttack.cs
@@ -0,0 +1,74 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+//This class just does almost everything right now
+public class PlayerAttack : MonoBehaviour
+{
+    public Animator playerAnimator; //animator is set though inspector in unity editor
+
+    public bool canAttack = true;
+
+    //Input
+    void TestInput()
+    {
+        if (Input.GetKeyDown("joystick button 1") || Input.GetKeyDown(KeyCode.K))
+        {
+
+            Attack();
+        }
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        TestInput();
+    }
+
+    // Combosystem
+    private int combocounter = 0;
+    int maxCombo = 5;
+
+
+    void Attack()
+    {
+        canAttack = false;
+        playerAnimator.SetBool("IsAttacking", true);
+        StartCoroutine(AttackCooldown());
+        //Debug.Log("combocounter: " + combocounter);
+        //if (BeatChecker.instance.IsInBeat())
+        //{
+        //    playerAnimator.SetTrigger("TAN" + (combocounter + 1)); // Test Attack Normal 1-5
+        //    combocounter = (combocounter + 1) % maxCombo;
+        //}
+        //else
+        //{
+        //    combocounter = 0; //reset combo
+        //    playerAnimator.SetTrigger("whiteN1"); // do weak attack
+        //}
+    }
+
+    IEnumerator AttackCooldown()
+    {
+        yield return new WaitForSeconds(0.25f);
+        canAttack = true;
+    }
+
+    //notizen des combosystems:
+    //maximal 5 angriffe
+    //jeder weitere angriff erhöht Schaden auf 3x
+    //dmg, 2dmg, 3dmg, 3dmg, 3dmg
+    //Angriffe 3,4,5 staggern
+    //Schwacher Angriff: 1/2 dmg
+
+    //beat miss: reset combo
+
+    //attack tipp - normaler angriff
+    //während combo beat auslassen - nächster normaler angriff ist salve
+    //x gedückt halten, bis zu drei beatlängen - Aufladeangriff
+    //x gedrückt halten und bewege input - aufgeladener bewegungsangriff
+
+    //nötige Animationen:
+    //schwach
+    //normal 1,2,3,4,5 mit steigernem schaden/intensity
+    //aufladeangriff, aufladeanimation, aufladeimpakt
+    //aufgeladener bewegungsangriff
+}
---
+++ Assets/Scripts/Player/PlayerMovement.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+
+    public CharacterController controller;
+    public Animator animator;
+
+    public float runSpeed = 40f;
+
+    float horizontalMove = 0f;
+    bool jump = false;
+
+    bool dashL = false;
+    bool dashR = false;
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        horizontalMove = Input.GetAxisRaw("Horizontal") * runSpeed;
+
+        animator.SetFloat("Speed", Mathf.Abs(horizontalMove));
+
+        if (Input.GetKeyDown("joystick button 0") || Input.GetKeyDown(KeyCode.Space))
+        {
+            jump = true;
+        }
+
+        //LT und RT --> Input Manager 
+        if (Input.GetAxis("LT") > 0 || Input.GetKeyDown(KeyCode.Q))
+        {
+            dashL = true;
+        }
+        if (Input.GetAxis("RT") > 0 || Input.GetKeyDown(KeyCode.E))
+        {
+            dashR = true;
+        }
+    }
+
+    public void OnFall()
+    {
+        animator.SetBool("IsJumping", true);
+    }
+
+    public void OnLanding()
+    {
+        animator.SetBool("IsJumping", false);
+    }
+
+    void FixedUpdate()
+    {
+        // Move our character
+        controller.Move(horizontalMove * Time.fixedDeltaTime, jump, dashL, dashR);
+        jump = false;
+        dashL = false;
+        dashR = false;
+    }
+}
