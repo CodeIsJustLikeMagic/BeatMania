5b24f17677b62acc2f2bd121a92d84d76195740e
DennisSchmidt23 <49645755+DennisSchmidt23@users.noreply.github.com>
2021-01-21 11:25:44 +0100

    Songchange via input possible

--- Assets/Scripts/Systems/SongchangeSystem.cs
+++ Assets/Scripts/Systems/SongchangeSystem.cs
@@ -5,6 +5,9 @@ using UnityEngine;
 public class SongchangeSystem : MonoBehaviour
 {
     public static SongchangeSystem instance;
+    public AudioHelm.AudioHelmClock clock;
+    public GameObject[] themes;
+    public int[] bpms;
     private int maxsongs = 2;
     [SerializeField]
     private int currentsong = 0;
@@ -19,13 +22,38 @@ public class SongchangeSystem : MonoBehaviour
     {
         SongChange(currentsong);
         //todo notify Clock that it's time to change song
-        currentsong = (currentsong + 1) % maxsongs;
+        //currentsong = (currentsong + 1) % maxsongs;
     }
     public void SongChange(int song)//gets called when user uses Songtree ?
     {
         Debug.Log("Songchanger songchange");
         Background.instance.SetSprites(song);
         SongSynchonizeVibing.instance.RecieveSongChange(song);
+        themes[currentsong].SetActive(false);
+        clock.bpm = bpms[song];
+        themes[song].SetActive(true);
+        currentsong = song;
         //todo notify Clock that it's time to change song
+    }
+
+    //einfach nur zum testen
+    public void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Alpha1)) 
+        {
+            SongChange(0);
+        }
+        if (Input.GetKeyDown(KeyCode.Alpha2))
+        {
+            SongChange(1);
+        }
+        if (Input.GetKeyDown(KeyCode.Alpha3))
+        {
+            SongChange(2);
+        }
+        if (Input.GetKeyDown(KeyCode.Alpha4))
+        {
+            SongChange(3);
+        }
     }
 }
