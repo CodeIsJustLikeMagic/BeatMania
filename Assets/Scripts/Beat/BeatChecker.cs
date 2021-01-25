55f7b93a3e5346b96add3f8d7e53b2bcf0a15404
Janselepoor <jan-felix@hotmail.com>
2021-01-25 14:57:30 +0100

    Beatchecker mit Latenzkallibrierung erweitert // vorerst in gui eingebaut

--- Assets/Scripts/Beat/BeatChecker.cs
+++ Assets/Scripts/Beat/BeatChecker.cs
@@ -3,15 +3,75 @@
 public class BeatChecker : VibingEntity
 {
     public static BeatChecker instance;
+
+    public float toleranceShift = 0.00f;
     public float toleranceRange = 0.07f;
 
-    /// <summary>
-    /// Checks if current time is in musik beat.
-    /// </summary>
-    /// <returns>true if Time.time is in beat</returns>
-    public bool IsInBeat()//is current time in beat?
+    //Hold Beat
+    float beatStart = 0;
+    float beatLength = 0;
+
+    //Callibration + UI
+    private bool toggle = false;
+    private int hitcount = 0;
+    private int misscount = 0;
+    private float missedBY = 0;
+    private float hitAVG = 0;
+    private bool isSet = false;
+
+    void Update()
     {
-        return IsInBeat(Time.time, toleranceRange);
+        //Toggle UI
+        if (Input.GetKeyDown(KeyCode.N))
+            {
+            toggle = !toggle;
+            if (toggle) {
+                //RESET SHIFT
+                toleranceShift = 0.00f;
+                hitcount = 0;
+                misscount = 0;
+                hitAVG = 0;
+                isSet = false;
+            }
+        }
+
+        //GiVE INPUT TO
+        if (Input.GetKeyDown(KeyCode.M)){
+            if (toggle && !isSet)
+            {
+                //missedBY = (Time.time - beatStart);
+                missedBY = (Time.time - beatStart) % beatLength;
+
+                if (missedBY <= (toleranceRange + toleranceShift))
+                {
+                    hitAVG += missedBY;
+                    hitcount = hitcount + 1;
+                }
+                else if (missedBY >= (beatLength - (toleranceRange - toleranceShift)))
+                {
+                    hitAVG -= (beatLength - missedBY);
+                    hitcount = hitcount + 1;
+                }
+                else
+                {
+                    misscount = misscount + 1;
+                }
+                if (hitcount == 10) {
+                    //sollte nur einmal setzen
+                    toleranceShift += (hitAVG / 10);
+                    isSet = true;
+                }
+            }
+        }
+    }
+
+        /// <summary>
+        /// Checks if current time is in musik beat.
+        /// </summary>
+        /// <returns>true if Time.time is in beat</returns>
+        public bool IsInBeat()//is current time in beat?
+    {
+        return IsInBeat(Time.time, toleranceRange, toleranceShift);
     }
 
     /// <summary>
@@ -20,11 +80,33 @@ public class BeatChecker : VibingEntity
     /// <param name="time"></param>
     /// <param name="toleranceRange"></param>
     /// <returns>true if time is in beat</returns>
-    public bool IsInBeat(float time, float toleranceRange)
+    public bool IsInBeat(float time, float toleranceRange, float toleranceShift)
     {
         // time is in beat if: time == beatStart + n * beatLegnth + -toleranceRange; where n is any natural number
         float missedBySeconds = (time - beatStart) % beatLength;
-        return missedBySeconds <= toleranceRange || missedBySeconds >= beatLength - toleranceRange;
+        return missedBySeconds <= (toleranceRange + toleranceShift) || missedBySeconds >= beatLength - (toleranceRange - toleranceShift);
+        //return missedBySeconds <= toleranceRange || missedBySeconds >= beatLength - toleranceRange; old
+    }
+
+
+    public void OnGUI()
+    {
+        if (toggle)
+        {
+            GUI.contentColor = Color.black;
+            GUI.Label(new Rect(10, 25, 200, 500), "HIT_COUNT: " + hitcount);
+            GUI.Label(new Rect(10, 50, 200, 500), "MISS_COUNT: " + misscount);
+            GUI.Label(new Rect(10, 75, 200, 500), "CURRENT_SHIFT: " + toleranceShift);
+            GUI.Label(new Rect(10, 100, 200, 500), "CURRENT_RANGE: " + toleranceRange);
+            GUI.Label(new Rect(10, 125, 200, 500), "BEAT: " + beatStart);
+            GUI.Label(new Rect(10, 150, 200, 500), "TIME: " + Time.time);
+            GUI.Label(new Rect(10, 175, 200, 500), "DIFF: " + (Time.time - beatStart));
+            GUI.Label(new Rect(10, 200, 200, 500), "MISSED_BY: " + missedBY);
+            GUI.Label(new Rect(10, 225, 200, 500), "HIT_AVG: " + hitAVG);
+            GUI.Label(new Rect(10, 250, 200, 500), "SHIFT_POS: " + (toleranceRange + toleranceShift));
+            GUI.Label(new Rect(10, 275, 200, 500), "SHIFT_NEG: " + (toleranceRange - toleranceShift));
+            GUI.Label(new Rect(10, 300, 200, 500), "ADJUSTED: " + isSet);
+        }
     }
 
     /// <summary>
@@ -34,7 +116,7 @@ public class BeatChecker : VibingEntity
     /// <returns>true if time is in beat</returns>
     public bool IsInBeat(float time)
     {
-        return IsInBeat(time, toleranceRange);
+        return IsInBeat(time, toleranceRange, toleranceRange);
     }
 
     public override void OnBeat(float bps)
@@ -48,7 +130,4 @@ public class BeatChecker : VibingEntity
         instance = this;
     }
 
-    //Hold Beat
-    float beatStart = 0;
-    float beatLength = 0;
 }
