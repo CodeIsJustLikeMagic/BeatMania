95cd4af648594ee66b3bb220ec2aae7af94e3c7f
CodeIsJustLikeMagic <janelle.pfeifer@freenet.de>
2021-03-18 17:34:17 +0100

    Turtle Alien Color adjusted and color change with song

---
+++ Assets/Scripts/Aliens/ChangeTexture.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChangeTexture : VibingEntity
+{
+    public Renderer renderer;
+    public Texture[] textures;
+    // Start is called before the first frame update
+    
+    void Awake()
+    {
+        if (renderer is null)
+        {
+            renderer = GetComponentInChildren<Renderer>();
+        }
+    }
+    public override void OnBeat(float bps)
+    {
+        return;
+    }
+    public override void OnSongChange(int song)
+    {
+        renderer.material.SetTexture("_MainTex", textures[song%textures.Length]);
+    }
+}
