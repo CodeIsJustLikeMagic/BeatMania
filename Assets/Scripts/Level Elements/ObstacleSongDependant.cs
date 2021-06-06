1585ce3e03a026ee0961c33ac2c072a9c735e6cf
CodeIsJustLikeMagic <janelle.pfeifer@freenet.de>
2021-06-07 01:22:18 +0200

    added shroomforest details + obstacles that block areas until songchange

---
+++ Assets/Scripts/Level Elements/ObstacleSongDependant.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObstacleSongDependant : VibingEntity
+{
+    [SerializeField] private bool[] openPassage = {true, true, true};
+    [SerializeField] private GameObject vis;
+    public override void OnSongChange(int song)
+    {
+        if (vis == null)
+        {
+            Debug.LogError("ObstaceSongDependant does not have a visualize object", this);
+            return;
+        }
+
+        vis.SetActive(openPassage[song % openPassage.Length]);
+    }
+
+    public override void OnBeat(float jitter_delay, float bps)
+    {
+        
+    }
+}
