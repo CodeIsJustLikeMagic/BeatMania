aa3f90ee7999ebe391622d130814580f2ac327c6
CodeIsJustLikeMagic <janelle.pfeifer@freenet.de>
2021-06-08 20:36:44 +0200

    fixed level 1. Rounded out character integration

--- Assets/Scripts/Level Elements/ObstacleSongDependant.cs
+++ Assets/Scripts/Level Elements/ObstacleSongDependant.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class ObstacleSongDependant : VibingEntity
 {
-    [SerializeField] private bool[] openPassage = {true, true, true};
+    [SerializeField] private bool[] passageBlocked = {true, true, true};
     [SerializeField] private GameObject vis;
     public override void OnSongChange(int song)
     {
@@ -14,7 +14,7 @@ public class ObstacleSongDependant : VibingEntity
             return;
         }
 
-        vis.SetActive(openPassage[song % openPassage.Length]);
+        vis.SetActive(passageBlocked[song % passageBlocked.Length]);
     }
 
     public override void OnBeat(float jitter_delay, float bps)
