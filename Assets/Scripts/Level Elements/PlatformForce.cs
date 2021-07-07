c4c5ce2e4e1a532a46492e94d28850601c930456
CodeIsJustLikeMagic <janelle.pfeifer@freenet.de>
2021-07-07 13:00:12 +0200

    player no longer falls through platform

---
+++ Assets/Scripts/Level Elements/PlatformForce.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Serialization;
+
+public class PlatformForce : MonoBehaviour
+{
+
+    public void OnCollisionEnter(Collision other)
+    {
+        if (other.gameObject.tag == "Player")
+        {
+            // make player our child
+            other.collider.transform.SetParent(transform);
+        }
+    }
+
+    public void OnCollisionExit(Collision other)
+    {
+        if (other.gameObject.tag == "Player")
+        {
+            other.collider.transform.SetParent(null);
+        }
+    }
+}
---
+++ Assets/Scripts/Level Elements/PlatformMoving.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlatformMoving : AlienBehavior
+{
+
+    [Tooltip("Understands: Up, Down, GoUp, GoDown, Invisible, Visible")]
+    [SerializeField]
+    private string[] combo = { "Up", "Up","Down", "Down", "Invisible", "Visible"}; //example combo
+
+    [SerializeField] private int move_every_x_beats = 1;
+    private int beat_num = 1;
+    private Animator animator;
+
+    private void Start()
+    {
+        animator = GetComponentInChildren<Animator>();
+
+    }
+
+    private int combocounter = 0;
+    public string debug = "";
+    public override void PerformBehaviorOnBeat(float bps)
+    {
+        if (beat_num < move_every_x_beats)
+        {
+            beat_num++;
+            return;
+        }
+
+        beat_num = 1;
+        if (animator == null)
+        {
+            animator = GetComponentInChildren<Animator>();
+        }
+
+        if (!animator.gameObject.activeSelf)
+        {
+            return;
+        }
+        animator.SetFloat("Speed", bps);
+        string move = combo[combocounter];
+        debug = move;
+        animator.SetTrigger(move);
+        combocounter = (combocounter + 1) % combo.Length;// cycle though combo array
+    }
+
+}
