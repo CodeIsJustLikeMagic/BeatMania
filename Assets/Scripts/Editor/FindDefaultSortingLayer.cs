51b03c4d42abb91d83a007c0bb55be6803028cd9
CodeIsJustLikeMagic <janelle.pfeifer@freenet.de>
2021-10-18 19:23:26 +0200

    fixed helmet disapearing behind trees

---
+++ Assets/Scripts/Editor/FindDefaultSortingLayer.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+
+public class FindDefaultSortingLayer : Editor
+{
+    [MenuItem("Tools/Find Sprite Renderers with Default Layer")]
+    private static void createColorChange()
+    {
+        var renderers = FindObjectsOfType<SpriteRenderer>();
+        List<Object> objects = new List<Object>();
+        foreach (SpriteRenderer r in renderers)
+        {
+            if (r.sortingLayerName == "Default")
+            {
+                objects.Add(r.gameObject);
+            }
+        }
+
+        Selection.objects = objects.ToArray();
+    }
+}
