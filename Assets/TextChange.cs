e295d31826d4ba533aaad904a82a39582b028838
CodeIsJustLikeMagic <janelle.pfeifer@freenet.de>
2021-11-11 18:53:56 +0100

    merge confict fixed

--- Assets/TextChange.cs
+++ Assets/TextChange.cs
@@ -18,7 +18,7 @@ public class TextChange : MonoBehaviour
 
     private void Awake()
     {
-        if (Input.GetJoystickNames().Length > 0) Controller = true;
+        //if (Input.GetJoystickNames().Length > 0) Controller = true;
         VersionAKeyboard = VersionAKeyboard.Replace("\\n", "\n");
         VersionBKeyboard = VersionBKeyboard.Replace("\\n", "\n");
         VersionAController = VersionAController.Replace("\\n", "\n");
