bb189f1b4a8e477306069b04eac4bb5a6423ba60
DennisSchmidt23 <49645755+DennisSchmidt23@users.noreply.github.com>
2021-11-11 13:04:37 +0100

    Added Texts for Game Variations

---
+++ Assets/TextChange.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class TextChange : MonoBehaviour
+{
+    public string VersionAKeyboard;
+    public string VersionBKeyboard;
+    public string VersionAController;
+    public string VersionBController;
+    bool Controller;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    private void Awake()
+    {
+        VersionAKeyboard = VersionAKeyboard.Replace("\\n", "\n");
+        VersionBKeyboard = VersionBKeyboard.Replace("\\n", "\n");
+        VersionAController = VersionAController.Replace("\\n", "\n");
+        VersionBController = VersionBController.Replace("\\n", "\n");
+
+        if (PlayerPrefs.GetInt("Version", 0) == 0)
+        {
+            if (!Controller)
+            {
+                
+                this.GetComponent<TextMeshPro>().SetText(VersionAKeyboard);
+            }
+            else
+            {
+                this.GetComponent<TextMeshPro>().SetText(VersionAController);
+            }
+            
+        }
+        else
+        {
+            if (!Controller)
+            {
+                this.GetComponent<TextMeshPro>().SetText(VersionBKeyboard);
+            }
+            else
+            {
+                this.GetComponent<TextMeshPro>().SetText(VersionBController);
+            }
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
