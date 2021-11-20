295e67bbcb97dd0b61e1f94c3a54efe3b8a8176b
CodeIsJustLikeMagic <janelle.pfeifer@freenet.de>
2021-11-20 15:06:57 +0100

    fixed bug where platforms that go up and turn invisible make the player fall through the floor

--- Assets/Scripts/Level Elements/PlatformForce.cs
+++ Assets/Scripts/Level Elements/PlatformForce.cs
@@ -7,12 +7,14 @@ using UnityEngine.Serialization;
 public class PlatformForce : MonoBehaviour
 {
 
+    private bool _playerIsStandingOnMe = false;
     public void OnCollisionEnter(Collision other)
     {
         if (other.gameObject.tag == "Player")
         {
             // make player our child
             other.collider.transform.SetParent(transform);
+            _playerIsStandingOnMe = true;
             // if the players transform is to low, which would mean not quite standing on the platform, glitch them on top
             if (-1f < other.gameObject.transform.position.y && other.gameObject.transform.position.y < 0.5f)
             {
@@ -22,11 +24,21 @@ public class PlatformForce : MonoBehaviour
         }
     }
 
+    private void OnDisable()
+    {
+        if (_playerIsStandingOnMe)
+        {
+            CharacterController.Instance.gameObject.transform.SetParent(null);
+            _playerIsStandingOnMe = false;
+        }
+    }
+
     public void OnCollisionExit(Collision other)
     {
         if (other.gameObject.tag == "Player")
         {
             other.collider.transform.SetParent(null);
+            _playerIsStandingOnMe = false;
         }
     }
 
